56ca8d72b44a502549a3d321b3bd6a5842d6bb93
samamiris <samaneh.amiri@gmail.com>
2023-02-07 21:53:53 -0500

    Update RbcApplication.java

--- src/main/java/com/challenge/rbc/RbcApplication.java
+++ src/main/java/com/challenge/rbc/RbcApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
+//@ComponentScan
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.challenge.rbc.repository")
 @EntityScan(basePackages = "com.challenge.rbc.entity")
